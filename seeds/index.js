4e5771e56eaa20818d93c44e167166748ea72032
southf09 <frankstonesouth@gmail.com>
2023-01-26 14:39:27 -0500

    Added function that clears the database, and generates a new database of length 50, containing the name of a random city from the cities database

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,8 @@
 //Require express, mongoose, and set paths
 const mongoose = require("mongoose");
-const Campground = require("./models/campground");
+const cities = require('./cities');
+const {places, descriptors} = ('./seedHelpers');
+const Campground = require("../models/campground");
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/campground");
@@ -11,6 +13,13 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
-const seedDB = async () => {
+const seedDb = async () => {
     await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() * 1000) + 1;
+        const newTest = new Campground({title: cities[random1000].city});
+        await newTest.save();
+    } 
 }
+
+seedDb();
