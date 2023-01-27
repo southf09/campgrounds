ead470d68badd90d9b252a2e93dd18bdd5954aa5
Frank South <frankstonesouth@gmail.com>
2023-01-27 11:03:43 -0500

    Fixed issue where mongo would not properly connect when using windows, due to changes in newer versions of mongodb

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/campground");
+mongoose.connect("mongodb://127.0.0.1:27017/campground");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
