b4e503a5b9a3a72133dff705ff51e7142fd03f5d
Frank South <frankstonesouth@gmail.com>
2023-01-31 16:19:08 -0500

    Set up basic route to display campgrounds

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
