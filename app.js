4e8f582763c3d6b5d22601ef3c1f43c0d2b6bc6b
southf09 <frankstonesouth@gmail.com>
2023-02-01 15:25:58 -0500

    Update portion of CRUD functionality working as expected now

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require('method-override');
 const Campground = require("./models/campground");
 
 mongoose.set("strictQuery", false);
@@ -23,6 +24,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 //Express listening on port 3000, and default route set up to test that express is working properly in the browser
 
@@ -56,6 +58,12 @@ app.get('/campgrounds/:id', async(req, res) => {
 app.get('/campgrounds/:id/edit', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+  res.redirect(`/campgrounds/${campground._id}`);
 })
 
 app.listen(3000, () => {
