71fd97834a03715f886c029d9780ee8e7a849e5c
southf09 <frankstonesouth@gmail.com>
2023-02-01 15:10:05 -0500

    CRUD functionality being added. Create/Read are done, Update is in progress, and Delete is next to come

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ app.get('/campgrounds/:id', async(req, res) => {
   res.render('campgrounds/show', { campground });
 });
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+})
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
